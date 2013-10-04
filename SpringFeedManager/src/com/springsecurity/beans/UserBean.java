b157b424900ad1c230299c68d32b170de272bf34
rafaelbp92 <rafaelbp92@gmail.com>
2013-10-04 11:45:12 -0300

    ajsuste lista de feeds

--- SpringFeedManager/src/com/springsecurity/beans/UserBean.java
+++ SpringFeedManager/src/com/springsecurity/beans/UserBean.java
@@ -1,6 +1,6 @@
 package com.springsecurity.beans;
 
-import java.io.IOException;
+
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
@@ -12,19 +12,18 @@ import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+
 
 import com.springsecurity.entities.Feed;
 import com.springsecurity.entities.RssEntry;
 import com.springsecurity.entities.User;
 import com.springsecurity.service.UserService;
-import com.sun.syndication.feed.synd.SyndContent;
-import com.sun.syndication.feed.synd.SyndContentImpl;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
+
 
 @Controller
 @Scope("request")
@@ -34,9 +33,9 @@ public class UserBean {
 	private UserService service;
 	@Autowired
 	private UserSession session;
+	private List<RssEntry> feeds;
 	
 	private String urlFeed;
-	private List<RssEntry> feeds;
 	
 
 	public String assinarFeed(){
@@ -49,15 +48,28 @@ public class UserBean {
 		return "";
 	}
 	
+	
+	private void message(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(message));
+	}
+	
+	public String getUrlFeed() {
+		return urlFeed;
+	}
+
+	public void setUrlFeed(String urlFeed) {
+		this.urlFeed = urlFeed;
+	}
+	
 	@SuppressWarnings("unchecked")
 	private void gerarListaFeeds(){	
 	        try {
 	          User user = session.getUser();
 	          List<Feed> feedsUrls = user.getFeeds();
-	          
+	          List<SyndEntry> entriesList = new ArrayList<SyndEntry>();
 	          for (Feed f : feedsUrls) {
 	        	  String rssFeedUrl = f.getUrl();
-			         
 			        SimpleDateFormat df =
 			                new SimpleDateFormat("dd/MM/yyyy");
 		            // Connect
@@ -66,13 +78,13 @@ public class UserBean {
 		            // Build the feed list
 		            SyndFeed feed = input.build(new XmlReader(feedUrl));
 		            feed.setEncoding("UTF-8");
-		            List<SyndEntry> feedList = feed.getEntries();
-		 
+		            List<SyndEntry> entries = feed.getEntries();
+		            entriesList.addAll(entries);
 		 
 		            feeds= new ArrayList<RssEntry>();
 		 
-		            for (SyndEntry entry : feedList) {
-		 
+		            for (SyndEntry entry : entriesList) {
+
 		 
 		                RssEntry rss = new RssEntry();
 		                 
@@ -101,25 +113,12 @@ public class UserBean {
 	         
 
 	}
-	
-	private void message(String message) {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(message));
-	}
-	
-	public String getUrlFeed() {
-		return urlFeed;
-	}
-
-	public void setUrlFeed(String urlFeed) {
-		this.urlFeed = urlFeed;
-	}
-
-	public List<RssEntry> getFeeds() throws IOException {
+	public List<RssEntry> getFeeds() {
 		gerarListaFeeds();
 		return feeds;
 	}
-	
+
+
 	public void setFeeds(List<RssEntry> feeds) {
 		this.feeds = feeds;
 	}
--- SpringFeedManager/src/com/springsecurity/beans/UserSession.java
+++ SpringFeedManager/src/com/springsecurity/beans/UserSession.java
@@ -1,16 +1,31 @@
 package com.springsecurity.beans;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 
+import com.springsecurity.entities.Feed;
+import com.springsecurity.entities.RssEntry;
 import com.springsecurity.entities.User;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 @Component
 @Scope("session")
 public class UserSession {
 	private User user;
-
+	
 	public User getUser() {
 		return user;
 	}
